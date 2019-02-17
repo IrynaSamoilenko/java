b93bca8394f30dab870272a91a5ce02bee284fc2
IrynaSamoilenko <iryna.python@gmail.com>
2019-02-17 09:40:25 +0200

    Lesson1 + HomeTask1 commit

---
+++ src/com/epam/hometasks/HomeTask1.java
@@ -0,0 +1,16 @@
+package com.epam.hometasks;
+
+/**
+ * Created by Samoilenko Iryna on 17.02.2019
+ */
+
+public class HomeTask1 {
+    public static void main(String[] args) {
+        System.out.println("* * * *  * * * *     *      *      *");
+        System.out.println("*        *     *    * *     * *  * *");
+        System.out.println("* *      * * * *   *   *    *   *  *");
+        System.out.println("*        *        * * * *   *      *");
+        System.out.println("* * * *  *       *       *  *      *");
+    }
+}
+
---
+++ src/com/epam/lessons/Lesson1.java
@@ -0,0 +1,19 @@
+package com.epam.lessons;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+//        System.out.println("Hello, what is your name?");
+//        Scanner sc = new Scanner(System.in);
+//        String userName = sc.nextLine();
+//        System.out.println("Nice to meet you, " + userName);
+//        System.out.println(" " +(8+3));
+//        System.err.println("Error Simple");
+        System.out.println("    *      *      *         *      *     ");
+        System.out.println("    *    *   *     *       *     *   *   ");
+        System.out.println("    *   *******     *     *     *******  ");
+        System.out.println("*   *  *       *      * *      *       * ");
+        System.out.println(" ***  *         *      *      *         *");
+
+
+    }
+}
