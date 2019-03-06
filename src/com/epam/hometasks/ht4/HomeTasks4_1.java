51e92de9ff607622aa9df921a8b9b08e59dcabe2
IrynaSamoilenko <iryna.python@gmail.com>
2019-03-06 16:43:41 +0200

    Lesson4 + HomeTask4 commit

--- src/com/epam/hometasks/ht4/HomeTasks4_1.java
+++ src/com/epam/hometasks/ht4/HomeTasks4_1.java
@@ -11,11 +11,27 @@ public class HomeTasks4_1 {
         System.out.println("Input text -> ");
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        System.out.println("The unique symbols of this string: " + uniqueChars(str) +'.');
+        System.out.println("The unique and DOESN'T DUPLICATE symbols of this string: " + uniqueChars(str) +'.');
+        System.out.println("The all unique symbols of this string: " + allUniqueChars(str) +'.');
 
     }
-
     public static String uniqueChars(String str) {
+        str = str.toLowerCase();
+        String storage = "";
+        int length = str.length();
+        int index = 0;
+        while (index < length) {
+            char ch = str.charAt(index++);
+            if (str.indexOf(ch) == str.lastIndexOf(ch)&&!storage.contains(String.valueOf(ch))){
+                storage += "'" + ch + "',";
+            } else {
+                storage="0. The string doesn't have unique and DOESN'T DUPLICATE symbols";
+            }
+        }
+        return storage;
+    }
+
+    public static String allUniqueChars(String str) {
         str = str.toLowerCase();
         String storage = "";
         int length = str.length();
